fb375729b562f1367fecdea5fd72dc8958070308
Abhi5738 <abhimanyukumawat7405@gmail.com>
2024-07-22 01:07:38 -0700

    Create react App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Testing Connect GitHub to Local </h1>
+    </div>
+  );
+}
+
+export default App;
